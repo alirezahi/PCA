ea70cbca9d8609360a4b84bb53ad1d88a25b973d
Alireza Heydari <alireza97hi@gmail.com>
2018-01-19 19:14:53 +0330

    samples got defined

--- PCA.py
+++ PCA.py
@@ -3,5 +3,8 @@ import numpy as np
 mean, covariance = [10,10] , [[4,4],[4,9]]
 number_of_samples = 1000
 
+samples = [[],[]]
+
 samples[0] = np.random.multivariate_normal(mean,covariance,number_of_samples)
 samples[1] = np.random.multivariate_normal(mean,covariance,number_of_samples)
+
